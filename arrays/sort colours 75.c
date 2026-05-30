95f3120607ae31cf2b23d154c1a6b60376e25c50
bhavya-sree1303 <bvemula@student.gitam.edu>
2026-05-30 22:18:38 +0530

    Update sort colours 75.c

--- arrays/sort colours 75.c
+++ arrays/sort colours 75.c
@@ -1,5 +1,15 @@
-#include <stdlib.h>
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+You must solve this problem without using the library's sort function.
+Example 1:
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+Input: nums = [2,0,1]
+Output: [0,1,2]
 
+    
+#include <stdlib.h>
 void merge(int* nums, int left, int mid, int right) {
     int n1 = mid - left + 1;
     int n2 = right - mid;
